c0515608847925d2094baedadadc30e515d8396f
abdullahzayed812 <aabdullah.zayed0099@gmail.com>
2025-01-30 00:33:18 +0200

    add deployed apis domain to the white list for cors

--- src/configs/corsOptions.ts
+++ src/configs/corsOptions.ts
@@ -1,6 +1,11 @@
 import { CorsOptions } from "cors";
 
-export const allowedOrigins = ["https://www.yoursite.com", "http://127.0.0.1:5500", "http://localhost:8081"];
+export const allowedOrigins = [
+  "https://www.yoursite.com",
+  "https://funnelapi.rentup.com.eg",
+  "http://127.0.0.1:5500",
+  "http://localhost:8081",
+];
 
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
