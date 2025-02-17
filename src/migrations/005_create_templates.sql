9cfb8db48f3e7058ecc973ae8053624164bed658
abdullah <aabdullah.zayed0099@gmail.com>
2025-02-17 15:18:52 +0200

    add template category column

--- src/migrations/005_create_templates.sql
+++ src/migrations/005_create_templates.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS templates (
   id                      VARCHAR(255) PRIMARY KEY,
   type                    ENUM('Default', 'Branded', 'Customized') NOT NULL,
+  category                VARCHAR(25) NOT NULL,
   tag                     VARCHAR(25) NOT NULL,
   name                    VARCHAR(255) NOT NULL,
   frame_svg               TEXT NOT NULL,
